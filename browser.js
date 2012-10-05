24ea26602dd7459b11498798d1ed5cf89be5c142
James Halliday <mail@substack.net>
2012-10-05 06:03:07 -0700

    show crosshairs in the browser

--- browser.js
+++ browser.js
@@ -6,8 +6,15 @@ var stream = shoe('/sock').pipe(JSONStream.parse([true]));
 var emitter = emitStream(stream);
 
 var img = document.querySelector('#viewer');
+var crosshairs = document.querySelector('#crosshairs');
 
 emitter.on('image', function (data) {
-console.log(data);
     img.setAttribute('src', 'data:image/png;base64,' + data);
+});
+
+emitter.on('red', function () {
+    crosshairs.className = 'active';
+    setTimeout(function () {
+        crosshairs.className = '';
+    }, 500);
 });
